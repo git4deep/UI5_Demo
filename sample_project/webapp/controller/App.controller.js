a93f54cca1aa73f69b27b20b9e39cc020ec7e53b
git4deep <81414947+git4deep@users.noreply.github.com>
2025-04-24 12:10:11 +0530

    Add files via upload

---
+++ sample_project/webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+], (BaseController) => {
+  "use strict";
+
+  return BaseController.extend("com.sampleproject.sampleproject.controller.App", {
+      onInit() {
+      }
+  });
+});
