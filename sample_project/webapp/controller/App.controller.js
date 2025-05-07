3965583a9ab3ae3598aadbe719ab160d813b189f
Deepak Maramittam <dmaramittam.aar@bloomholding.com>
2025-05-07 11:26:17 +0000

    Initial commit

--- sample_project/webapp/controller/App.controller.js
+++ sample_project/webapp/controller/App.controller.js
@@ -2,7 +2,7 @@ sap.ui.define([
   "sap/ui/core/mvc/Controller"
 ], (BaseController) => {
   "use strict";
-
+ 
   return BaseController.extend("com.sampleproject.sampleproject.controller.App", {
       onInit() {
       }
